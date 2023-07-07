e2b0c824955c47a5d04f5f3636d5d637047ecddb
Wackyator <pratapsinghtarun9@gmail.com>
2023-07-07 18:22:31 +0530

    feat: commit

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,38 @@
 
 mod types;
 
-use std::env;
+use std::{env, process::exit};
 
-use types::{Blob, FileService, Result};
+use types::{Blob, Commit, Error, FileService, Result};
 
 fn main() {
-    FileService::init_blip("/home/toaster/code/p/rust/blip");
-    add_file(vec![".gitignore"]);
+    // FileService::init_blip("/home/toaster/code/p/rust/blip").unwrap();
+    // add_file(vec![".gitignore"]).unwrap();
+    commit("commit text").unwrap();
+    // let fs = FileService::new().unwrap();
+    // println!("{:?}", fs.get_head_ref());
+}
+
+fn commit(msg: &str) -> Result<()> {
+    let file_service = FileService::new()?;
+    let head_ref = file_service.get_head_ref()?;
+    let parent_hash = FileService::get_hash_from_ref(&head_ref);
+    let mut index = file_service.read_index()?;
+
+    let parent = match parent_hash {
+        Some(hash) => Some(file_service.read_commit(&hash)?),
+        None => None,
+    };
+
+    let mut commit = Commit::new(parent.as_ref());
+    parent.map(|p| p.print());
+    commit.add_from_index(&index);
+    commit.print();
+
+    file_service.write_commit(&mut commit)?;
+    index.clear()?;
+    println!("{msg}");
+    Ok(())
 }
 
 fn add_file(files: Vec<&str>) -> Result<()> {
--- src/types.rs
+++ src/types.rs
@@ -3,10 +3,12 @@ use std::{
     env, fmt,
     fs::{self, File},
     io::{self, BufRead, BufReader, Read, Write},
+    os::unix::prelude::FileExt,
     path::{Path, PathBuf},
 };
 
 use crypto::{digest::Digest, sha1::Sha1};
+use regex::Regex;
 
 pub type Result<T> = core::result::Result<T, Error>;
 
@@ -15,6 +17,8 @@ pub enum Error {
     IoError(io::Error),
     NoDirectory,
     InvalidIndex,
+    InvalidObjectStore,
+    EmptyCommit,
 }
 
 #[derive(Debug)]
@@ -43,6 +47,7 @@ pub struct FileService {
     pub blip_dir: PathBuf,
     pub object_dir: PathBuf,
     pub index: PathBuf,
+    pub head: PathBuf,
 }
 
 #[derive(Debug)]
@@ -51,12 +56,22 @@ pub struct Index {
     pub hashtree: BTreeMap<String, String>,
 }
 
+#[derive(Debug)]
+pub struct Commit {
+    hash: Option<String>,
+    data: Option<Vec<u8>>,
+    parent: Option<String>,
+    files: BTreeMap<String, String>,
+}
+
 impl fmt::Display for Error {
     fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Self::IoError(e) => e.fmt(fmt),
             Self::NoDirectory => fmt.write_str("No Directory Found"),
             Self::InvalidIndex => fmt.write_str("Index is Corrupt"),
+            Self::InvalidObjectStore => fmt.write_str("Blip Repository is Corrupt"),
+            Self::EmptyCommit => fmt.write_str("No Files Staged for Commit"),
         }
     }
 }
@@ -100,12 +115,14 @@ impl FileService {
         let blip_dir = root_dir.join(".blip");
         let object_dir = blip_dir.join("objects");
         let index = blip_dir.join("index");
+        let head = blip_dir.join("HEAD");
 
         Ok(FileService {
             root_dir,
             blip_dir,
             object_dir,
             index,
+            head,
         })
     }
 
@@ -143,6 +160,39 @@ impl FileService {
 }
 
 impl FileService {
+    pub fn get_head_ref(&self) -> Result<PathBuf> {
+        let mut head_file = File::open(self.head.clone())?;
+        let mut ref_path = String::new();
+        head_file.read_to_string(&mut ref_path)?;
+        let ref_path = ref_path.split_off(5);
+
+        Ok(self.blip_dir.join(ref_path))
+    }
+
+    pub fn get_hash_from_ref(ref_path: &PathBuf) -> Option<String> {
+        match File::open(ref_path) {
+            Ok(mut f) => {
+                let mut hash = String::new();
+                f.read_to_string(&mut hash)
+                    .expect("Error: Ref File is Corrupt");
+                return Some(hash);
+            }
+            Err(_) => None,
+        }
+    }
+
+    pub fn read_commit(&self, hash: &str) -> Result<Commit> {
+        Commit::from(hash, &self.read_object(hash)?)
+    }
+
+    fn read_object(&self, hash: &str) -> Result<String> {
+        let mut data = String::new();
+        let mut object_file = File::open(self.object_dir.join(hash))?;
+        object_file.read_to_string(&mut data)?;
+
+        Ok(data)
+    }
+
     pub fn read_index(&self) -> Result<Index> {
         let mut index_data = BTreeMap::new();
 
@@ -159,11 +209,30 @@ impl FileService {
         Ok(Index::new(self.index.clone(), index_data))
     }
 
+    pub(crate) fn write_commit(&self, commit: &mut Commit) -> Result<()> {
+        commit.update();
+
+        match commit {
+            &mut Commit {
+                hash: Some(ref hash),
+                data: Some(ref data),
+                ..
+            } => {
+                self.write_obj(hash, data)?;
+                let mut head_file = File::create(self.get_head_ref()?)?;
+                head_file.write_all(hash.as_bytes())?;
+            }
+            _ => {
+                return Err(Error::EmptyCommit);
+            }
+        }
+
+        Ok(())
+    }
+
     pub fn write_index(&self, index: &Index) -> Result<()> {
         let mut file = File::create(self.index.clone())?;
         for (hash, path) in index.hashtree().iter() {
-            println!("writing: {hash}, {path}");
-            // file.write_all(format!("{} {}", hash, path).as_bytes())?;
             writeln!(&mut file, "{} {}", hash, path);
         }
         Ok(())
@@ -194,5 +263,115 @@ impl Index {
 
     pub fn update(&mut self, path: &str, hash: &str) {
         self.hashtree.insert(path.to_string(), hash.to_string());
+    }
+
+    pub(crate) fn clear(&mut self) -> Result<()> {
+        self.hashtree = BTreeMap::new();
+        self.write()?;
+        Ok(())
+    }
+
+    fn write(&self) -> Result<()> {
+        let mut index = File::create(&self.path)?;
+        for (hash, path) in self.hashtree.iter() {
+            writeln!(&mut index, "{hash} {path}");
+        }
+        Ok(())
+    }
+}
+
+impl Commit {
+    pub fn new(parent: Option<&Commit>) -> Commit {
+        let mut commit = Commit {
+            hash: None,
+            data: None,
+            parent: match parent {
+                Some(&Commit {
+                    hash: Some(ref hash),
+                    ..
+                }) => Some(hash.to_string()),
+                _ => None,
+            },
+            files: BTreeMap::new(),
+        };
+
+        for (hash, path) in parent.iter().flat_map(|p| p.files.iter()) {
+            commit.files.insert(hash.to_string(), path.to_string());
+        }
+
+        commit
+    }
+
+    pub fn from(hash: &str, input: &str) -> Result<Commit> {
+        let mut commit = Commit::new(None);
+        commit.hash = Some(hash.to_string());
+
+        let parent = Regex::new(r"parent ([0-9a-f]{40})").unwrap();
+        let blob = Regex::new(r"blob ([0-9a-f]{40}) (.*)").unwrap();
+
+        for line in input.lines() {
+            if let Some(caps) = parent.captures(line) {
+                // this syntax is ugly looking but is definitely better than panicing imo
+                // alternate way to do this would be
+                // commit.parent = Some(caps.get().unwrap().as_str().into());
+                // or
+                // commit.parent = Some(caps.get(1).ok_or_else(|| Error::InvalidObjectStore)?.as_str().into());
+                // if-let-else can be used together to all this at once but looks cluttered
+                let Some(hash) = caps.get(1) else {
+                    return Err(Error::InvalidObjectStore);
+                };
+                commit.parent = Some(hash.as_str().into());
+            }
+
+            if let Some(caps) = blob.captures(line) {
+                let Some(hash) = caps.get(1) else {
+                    return Err(Error::InvalidObjectStore);
+                };
+                let Some(ref path) = caps.get(3) else {
+                    return Err(Error::InvalidObjectStore);
+                };
+
+                commit
+                    .files
+                    .insert(hash.as_str().to_string(), path.as_str().to_string());
+            }
+        }
+
+        Ok(commit)
+    }
+}
+
+impl Commit {
+    pub(crate) fn print(&self) {
+        if let Some(ref parent) = self.parent {
+            println!("parent {parent}");
+        }
+
+        for (hash, path) in self.files.iter() {
+            println!("blob {hash} {path}");
+        }
+    }
+
+    pub(crate) fn add_from_index(&mut self, index: &Index) {
+        for (hash, path) in index.hashtree().iter() {
+            self.files.insert(hash.to_string(), path.to_string());
+        }
+    }
+
+    pub(crate) fn update(&mut self) {
+        let mut data: Vec<u8> = Vec::new();
+
+        if let Some(ref parent) = self.parent {
+            writeln!(&mut data, "parent {parent}");
+        }
+
+        for (hash, path) in self.files.iter() {
+            writeln!(&mut data, "blob {hash}, {path}");
+        }
+
+        let mut hash = Sha1::new();
+        hash.input(&data);
+        self.hash = Some(hash.result_str());
+        self.data = Some(data);
     }
 }
